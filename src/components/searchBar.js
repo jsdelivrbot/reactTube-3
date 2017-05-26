394873ac673149969c2cb89bccd25fc0906067eb
Adam Kaźmierczak <adam.kazmierczak91@gmail.com>
2017-05-26 19:54:17 +0200

    adding files to basic structure

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                I'm a search bar!'
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
