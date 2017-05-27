cafc7b85d108d7147be7ce5318b4a9912cff8c94
Adam Kaźmierczak <adam.kazmierczak91@gmail.com>
2017-05-27 18:56:33 +0200

    searchBar component logic

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+    }
+
     render() {
         return (
-            <div>
-                I'm a search bar!'
+            <div className="form-group">
+                <input className="form-control" value={this.state.term} type="text" placeholder="Search for a term"  onChange={event => this.onInputChange(event.target.value)} />
             </div>
         )
+    }
+
+    onInputChange(term) {
+        this.setState({
+            term
+        });
+        this.props.onSearchTermChange(term);
     }
 }
 
